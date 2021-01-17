96ec76397c151a5d7cefe2ecbd05a6839cb6897b
AlexApps99 <alex.apps99@gmail.com>
2021-01-18 12:54:26 +1300

    Fix packaging

--- cspice-sys/build.rs
+++ cspice-sys/build.rs
@@ -1,12 +1,10 @@
 fn main() {
     #[cfg(feature = "generate")]
     let out_dir = std::path::PathBuf::from(std::env::var("OUT_DIR").unwrap());
-    let mut wk_dir = std::path::PathBuf::from(std::env::var("CARGO_MANIFEST_DIR").unwrap());
-    wk_dir.pop();
 
     #[cfg(feature = "generate")]
     bindgen::Builder::default()
-        .header(wk_dir.join("cspice/SpiceUsr.h").to_str().unwrap())
+        .header("cspice/SpiceUsr.h")
         .parse_callbacks(Box::new(bindgen::CargoCallbacks))
         .whitelist_function(".*_c")
         .whitelist_type("Spice.*")
@@ -16,6 +14,7 @@ fn main() {
         .write_to_file(out_dir.join("cspice.rs"))
         .expect("Unable to save bindings");
 
-    println!("cargo:rustc-link-search=native={}", wk_dir.join("cspice/").to_str().unwrap());
+    println!("cargo:rustc-link-search=native=cspice-sys/cspice/");
+    println!("cargo:rustc-link-search=native=cspice/"); // cargo publish
     println!("cargo:rustc-link-lib=static=cspice");
 }
